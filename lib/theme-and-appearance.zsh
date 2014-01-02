7557300ade7044a332c45c5008df00039f2e8ac7
tinyladi <ladislas@weareleka.com>
2014-01-02 23:11:56 +0100

    customize

--- lib/theme-and-appearance.zsh
+++ lib/theme-and-appearance.zsh
@@ -4,21 +4,29 @@ export LSCOLORS="Gxfxcxdxbxegedabagacad"
 #export LS_COLORS
 
 # Enable ls colors
+# Edited - S.M. Changed ls -G -> ls --color. Requires dircolor to be valid.
 if [ "$DISABLE_LS_COLORS" != "true" ]
 then
   # Find the option for using colors in ls, depending on the version: Linux or BSD
-  if [[ "$(uname -s)" == "NetBSD" ]]; then
-    # On NetBSD, test if "gls" (GNU ls) is installed (this one supports colors); 
-    # otherwise, leave ls as is, because NetBSD's ls doesn't support -G
-    gls --color -d . &>/dev/null 2>&1 && alias ls='gls --color=tty'
-  elif [[ "$(uname -s)" == "OpenBSD" ]]; then
-    # On OpenBSD, test if "colorls" is installed (this one supports colors);
-    # otherwise, leave ls as is, because OpenBSD's ls doesn't support -G
-    colorls -G -d . &>/dev/null 2>&1 && alias ls='colorls -G'
-  else
-    ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
-  fi
+  ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls --color'
 fi
+
+# # Enable ls colors
+# if [ "$DISABLE_LS_COLORS" != "true" ]
+# then
+#   # Find the option for using colors in ls, depending on the version: Linux or BSD
+#   if [[ "$(uname -s)" == "NetBSD" ]]; then
+#     # On NetBSD, test if "gls" (GNU ls) is installed (this one supports colors);
+#     # otherwise, leave ls as is, because NetBSD's ls doesn't support -G
+#     gls --color -d . &>/dev/null 2>&1 && alias ls='gls --color=tty'
+#   elif [[ "$(uname -s)" == "OpenBSD" ]]; then
+#     # On OpenBSD, test if "colorls" is installed (this one supports colors);
+#     # otherwise, leave ls as is, because OpenBSD's ls doesn't support -G
+#     colorls -G -d . &>/dev/null 2>&1 && alias ls='colorls -G'
+#   else
+#     ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+#   fi
+# fi
 
 #setopt no_beep
 setopt auto_cd
